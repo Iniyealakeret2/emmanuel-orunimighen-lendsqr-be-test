8e3ee602711daef4340a963ccab3d9e24c0c1f6b
Emmanuel Orunimighen <iniyealakeret1@gmail.com>
2024-07-01 15:06:18 +0100

    added knex config file

---
+++ knexfile.ts
@@ -0,0 +1,40 @@
+import path from "path";
+import type { Knex } from "knex";
+
+import config from "./src/config";
+
+const knexConfig: { [key: string]: Knex.Config } = {
+  development: {
+    client: "mysql2",
+    connection: {
+      host: config.DB_HOST,
+      user: config.DB_USER,
+      password: config.DB_PASSWORD,
+      database: config.DATABASE_NAME,
+    },
+    migrations: {
+      directory: path.resolve(__dirname, "./database/migrations"),
+    },
+    seeds: {
+      directory: path.resolve(__dirname, "./database/seeds"),
+    },
+  },
+
+  production: {
+    client: "mysql",
+    connection: {
+      host: config.DB_HOST,
+      user: config.DB_USER,
+      password: config.DB_PASSWORD,
+      database: config.DB_NAME,
+    },
+    migrations: {
+      directory: path.resolve(__dirname, "./database/migrations"),
+    },
+    seeds: {
+      directory: path.resolve(__dirname, "./database/seeds"),
+    },
+  },
+};
+
+export default knexConfig;
